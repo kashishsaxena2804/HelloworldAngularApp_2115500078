f2b5a0a724aa96289268e21036ea7f36ac6404e5
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-03-21 18:20:59 +0530

    UC5 Ensure the userName is the valid input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,24 @@ export class AppComponent {
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url= "https:www.bridgelabz.com";
   userName: string= "";
-  
+  nameError: string= ""; 
 
   ngOnInit(): void{
     this.title="Hello from BridgeLabz.";
   }
-  onClick($event:Event){
+  onClick($event: Event) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+  
+  onInput($event: any) {
+    console.log("Change Event Occurred!", $event.target.value);
+    const nameRegex = new RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
+  }
+  
 }
