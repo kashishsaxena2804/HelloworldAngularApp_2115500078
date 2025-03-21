ba06e2c3198ce91ce254382ee000c2bd907c57bf
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-03-21 17:03:33 +0530

    UC3 Ability To launch BridgeLabz Site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Helloworld';
   imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url= "https:www.bridgelabz.com"
 
   ngOnInit(): void{
     this.title="Hello from BridgeLabz.";
+  }
+  onClick($event:Event){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
